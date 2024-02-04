2ea0219f93097def7fc35d20d9480ac8bc20b00e
Jeremy Demortier <jeremy.demortier@gmail.com>
2024-02-04 15:36:16 +0100

    Work on AccountEditionView

--- src/Ingots.Avalonia/Views/AccountEditionView.axaml.cs
+++ src/Ingots.Avalonia/Views/AccountEditionView.axaml.cs
@@ -3,6 +3,10 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using Ingots.ViewModels;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace Ingots.Avalonia.Views;
 
@@ -11,5 +15,16 @@ public partial class AccountEditionView : ReactiveUserControl<AccountViewModel>
     public AccountEditionView()
     {
         InitializeComponent();
+
+        this.WhenActivated( disposables =>
+        {
+            this.WhenAnyValue( x => x.ViewModel )
+                .WhereNotNull()
+                .Do( vm => PopulateFromViewModel( this , vm , disposables ) )
+                .Subscribe()
+                .DisposeWith( disposables );
+        } );
     }
+    
+    private static void PopulateFromViewModel(AccountEditionView view,AccountViewModel viewModel,CompositeDisposable disposables){}
 }
